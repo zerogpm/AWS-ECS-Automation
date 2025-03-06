062763d5657bac6ebb1a89babe67ef5038866ff3
Chris Su <zerogpm@gmail.com>
2025-03-06 11:08:31 -0800

    add ecs task sgs output

--- modules/security/outputs.tf
+++ modules/security/outputs.tf
@@ -3,3 +3,8 @@ output "loadbalancer_security_group_id" {
   description = "The ID of the Load Balancer Security Group"
   value       = module.loadbalancer_sg.security_group_id
 }
+
+output "ecs_task_security_group_id" {
+  description = "The ID of the ECS task Security Group"
+  value       = module.ecs_task_sg.security_group_id
+}
