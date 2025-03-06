d4bc2e611fa83d2fab98d92bffc353c5c6048f19
Chris Su <zerogpm@gmail.com>
2025-03-06 11:04:20 -0800

    add place

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -3,7 +3,6 @@
 module "loadbalancer_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
-
   name        = "loadbalancer-sg"
   description = "Security Group with HTTP open for entire Internet (IPv4 CIDR), egress ports are all world open"
   vpc_id      = var.vpc_id
@@ -13,24 +12,6 @@ module "loadbalancer_sg" {
   # Egress Rule - all-all open
   egress_rules = ["all-all"]
   tags         = var.tags
-
-  # Another way to define mutiple points which base on SGs
-  #   ingress_with_source_security_group_id = [
-  #     {
-  #       from_port                = 80
-  #       to_port                  = 80
-  #       protocol                 = "tcp"
-  #       description              = "Allow HTTP from ALB"
-  #       source_security_group_id = module.alb_sg.security_group_id
-  #     },
-  #     {
-  #       from_port                = 81
-  #       to_port                  = 81
-  #       protocol                 = "tcp"
-  #       description              = "Allow Port 81 from ALB"
-  #       source_security_group_id = module.alb_sg.security_group_id
-  #     }
-  #   ]
 }
 
 module "ecs_task_sg" {
@@ -40,8 +21,7 @@ module "ecs_task_sg" {
   name        = "ecs-task-sg"
   description = "Security Group with port 3000 open for entire Internet (IPv4 CIDR), egress ports are all world open"
   vpc_id      = var.vpc_id
-
-
+  
   ingress_with_cidr_blocks = [
     {
       from_port   = 3000
